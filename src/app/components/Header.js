50ede55b0f91a05d1ed95aa7a0d5db4f7fb2f951
Gabriel Braga <gabribragandrade@gmail.com>
2025-07-08 20:10:03 -0300

    Main Banner

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -57,9 +57,9 @@ const Header = () => {
   return (
     <header
       // Classes Tailwind para transição fluida, posição fixa e sombra
-      className={`fixed top-0 left-0 w-full z-50 p-4 transition-all duration-300 ease-in-out shadow-md ${headerBg}`}
+      className={`fixed top-0 left-0 w-full z-50 p-4 transition-all duration-300 ease-in-out shadow-md flex justify-center items-center ${headerBg}`}
     >
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container w-[1200px] flex items-center justify-between">
         {/* Logo */}
         <div className="flex-shrink-0">
           <img
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,16 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen bg-palette-5 text-palette-3">
-      <div className="w-full flex items-center justify-center bg-[url('/silence-is-violence.jpg')] bg-size-[auto_250%] bg-no-repeat bg-position-[left_100%_center] h-[600px]">
-        <div className="w-[1200px] h-96">
+      <div className="w-full flex items-center justify-center mt-24">
+        <div className="w-[1200px] h-[500px] bg-[url('/silence-is-violence.jpg')] bg-size-[auto_250%] bg-position-[left_70%_center] rounded-4xl shadow-lg overflow-hidden">
+          <div className="bg-linear-to-l from-palette-3 to-transparent from-10% h-full w-full flex flex-col items-end justify-center text-palette-5 p-12 gap-10">
+            <h1 className="text-6xl font-semibold">Silêncio é Violência!</h1>
+            <p className="max-w-96 text-end">Não fique em <b>silêncio</b> diante da opressão. Sua voz é importante! Aprenda a se manifestar ou entre em contato com a gente.</p>
+            <div className="flex items-center justify-end gap-5">
+              <button className="bg-transparent border-2 border-palette-5 text-palette-5 py-2 px-4 rounded-full cursor-pointer hover:bg-palette-5 hover:text-palette-3 transition-colors ease-in-out duration-200">Aprender a se manifestar</button>
+              <button className="bg-palette-5 text-palette-3 py-2 px-4 rounded-full cursor-pointer hover:bg-palette-1 transition-colors ease-in-out duration-200">Fazer uma denúncia</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
