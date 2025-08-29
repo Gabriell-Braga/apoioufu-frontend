2ab43f6d70b2143a1d3177441d5c0a0d1ebe47f0
Gabriel Braga <gabribragandrade@gmail.com>
2025-08-29 11:44:58 -0300

    correção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ const HomePage = () => {
                 </div>
                 <div style={{ display: 'none' }}>
                     <Image
-                    src={'/silence-is-violence.avif'}
+                    src={'public/silence-is-violence.avif'}
                     alt="LCP Preload Image"
                     priority 
                     width={1} 
